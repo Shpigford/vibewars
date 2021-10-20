c0039ccf2338468548f0d2d716013dd612e1d453
Josh Pigford <josh@joshpigford.com>
2021-10-20 16:42:06 -0500

    Hacky dropdown

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :load_all_collections
+
+  def load_all_collections
+      @all_collections = Collection.all.order(name: :asc)
+  end
+
 end
