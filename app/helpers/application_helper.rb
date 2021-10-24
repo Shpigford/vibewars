740310bac6a77662ad35c9d5f00dadd08dafb332
Josh Pigford <josh@joshpigford.com>
2021-10-23 23:12:11 -0500

    Title/Description meta tags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,16 @@ module ApplicationHelper
       request.user_agent =~ /Mobile|webOS/
     end
   end
+
+  def title(page_title)
+    content_for(:title) { page_title }
+  end
+
+  def description(page_description)
+    content_for(:description) { page_description }
+  end
+
+  def meta_image(meta_image)
+    content_for(:meta_image) { meta_image }
+  end
 end
