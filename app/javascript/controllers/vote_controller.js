b3768b6698f79e5b6f6b0e2deb7d0583cdbc173b
Josh Pigford <josh@joshpigford.com>
2021-10-28 13:37:56 -0500

    Use arrow keys to vote
    Resolves #19

--- app/javascript/controllers/vote_controller.js
+++ app/javascript/controllers/vote_controller.js
@@ -2,11 +2,26 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="vote"
 export default class extends Controller {
-  static targets = ["element"];
+  static targets = ["element", "itemFirst", "itemLast"];
 
   toggle(event) {
     //event.preventDefault();
     event.target.classList.add("opacity-100");
     event.target.classList.remove("opacity-0");
+  }
+
+  keypress(event) {
+    if (event.key == 'ArrowLeft') {
+      this.itemFirstTarget.querySelector('.voted').classList.add("opacity-100");
+      this.itemFirstTarget.querySelector('.voted').classList.remove("opacity-0");
+
+      this.itemFirstTarget.click();
+    }
+    else if (event.key == 'ArrowRight') {
+      this.itemLastTarget.querySelector('.voted').classList.add("opacity-100");
+      this.itemLastTarget.querySelector('.voted').classList.remove("opacity-0");
+
+      this.itemLastTarget.click();
+    }
   }
 }
