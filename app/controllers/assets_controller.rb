bf8f8c5b6998efcaccdf587504c972658449b3d8
Josh Pigford <josh@joshpigford.com>
2021-10-28 15:38:36 -0500

    Wrong collection

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -1,6 +1,6 @@
 class AssetsController < ApplicationController
   def show
     @collection = Collection.where(slug: params[:collection_id]).first
-    @asset = Asset.where(token_id: params[:id]).first
+    @asset = Asset.where(collection_id: @collection.id, token_id: params[:id]).first
   end
 end
