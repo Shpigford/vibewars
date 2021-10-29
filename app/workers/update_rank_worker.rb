2902e5014d300a3a56535f7f97d10c02f8936068
Josh Pigford <josh@joshpigford.com>
2021-10-29 11:12:42 -0500

    Optimize rank updating

--- app/workers/update_rank_worker.rb
+++ app/workers/update_rank_worker.rb
@@ -2,10 +2,11 @@ class UpdateRankWorker
   include Sidekiq::Worker
 
   def perform(collection_id)
-    collection = Collection.find(collection_id)
-
-    collection.assets.order(elo_rating: :desc).each_with_index do |asset, index|
-      asset.update(rank: index + 1);
-    end
+    ActiveRecord::Base.connection.execute("UPDATE assets a
+    SET rank = a2.seqnum
+    from (select a2.*, row_number() over (ORDER BY elo_rating DESC) as seqnum
+          from assets a2 WHERE a2.collection_id = #{collection_id}
+         ) a2
+    where a2.id = a.id;");
   end
 end
