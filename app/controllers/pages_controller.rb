fdfc3fa22eb849ab2eb130af947290cc78616388
Josh Pigford <josh@joshpigford.com>
2021-10-31 15:53:42 -0500

    Leaderboard page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,8 @@ class PagesController < ApplicationController
       "name ILIKE :search OR description ILIKE :search OR symbol ILIKE :search", search: "%#{params[:term]}%"
     )
   end
+
+  def leaderboard
+    @leaders = Vote.where.not(wallet_id: nil).group(:wallet_id).order(count_all: :desc).count
+  end
 end
