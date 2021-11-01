1bc538715883f4ec5af0184d9a546fefd700e3fc
Josh Pigford <josh@joshpigford.com>
2021-10-31 21:07:00 -0500

    Pull collection find into method

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,16 +1,11 @@
 class CollectionsController < ApplicationController
+  before_action :get_collection, except: [:index]
+
   def index
     @collections = Collection.all.order(name: :asc)
   end
 
   def show
-    if /\A0x[a-fA-F0-9]{40}\z/i.match(params[:id])
-      @collection = Collection.where(address: params[:id]).first
-      redirect_to collection_path(@collection.slug)
-    else
-      @collection = Collection.where(slug: params[:id]).first
-    end
-
     compare = @collection.assets.order(votes_count: :asc).limit(50)
     @item_first = compare.sample
     @item_last = compare.sample
@@ -21,19 +16,21 @@ class CollectionsController < ApplicationController
   end
 
   def ranking
+    @assets = @collection.assets.where('rank > 0').order(rank: :asc).page params[:page]
+  end
+
+  def leaderboard
+    @leaders = @collection.votes.where.not(wallet_id: nil).group(:wallet_id).order(count_all: :desc).count
+  end
+
+private
+  def get_collection
     if /\A0x[a-fA-F0-9]{40}\z/i.match(params[:id])
       @collection = Collection.where(address: params[:id]).first
       redirect_to ranking_collection_path(@collection.slug)
     else
       @collection = Collection.where(slug: params[:id]).first
     end
-
-    @assets = @collection.assets.where('rank > 0').order(rank: :asc).page params[:page]
   end
-
-  def leaderboard
-    @collection = Collection.where(slug: params[:id]).first
-
-    @leaders = @collection.votes.where.not(wallet_id: nil).group(:wallet_id).order(count_all: :desc).count
-  end
+  
 end
