efdeae82e21394072eec42812b25476dcb77d726
Josh Pigford <josh@joshpigford.com>
2021-11-05 19:51:07 -0400

    Trait rarity

--- app/helpers/collections_helper.rb
+++ app/helpers/collections_helper.rb
@@ -1,2 +1,6 @@
 module CollectionsHelper
+  def trait_rarity(collection_total, trait_total)
+    percentage = ((trait_total.to_f / collection_total.to_f) * 100).round(1)
+    percentage.to_s + "%"
+  end
 end
