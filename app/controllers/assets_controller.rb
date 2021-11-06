0f3320706b45d1d732e589cd64ffb4fab95909ab
Josh Pigford <josh@joshpigford.com>
2021-11-06 13:30:32 -0400

    JSON response for assets

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -2,5 +2,10 @@ class AssetsController < ApplicationController
   def show
     @collection = Collection.where(slug: params[:collection_id]).first
     @asset = Asset.where(collection_id: @collection.id, token_id: params[:id]).first
+
+    respond_to do |format|
+      format.html
+      format.json { render json: { collection: @collection, asset: @asset} }
+    end
   end
 end
