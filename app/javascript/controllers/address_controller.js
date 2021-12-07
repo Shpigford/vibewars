236d5d3b8c7c7f38ffe96db5e5614a452c086c5c
Josh Pigford <josh@joshpigford.com>
2021-12-07 17:38:16 -0600

    Regex tweak again

--- app/javascript/controllers/address_controller.js
+++ app/javascript/controllers/address_controller.js
@@ -8,6 +8,6 @@ export default class extends Controller {
 
   async addressValueChanged() {
     const ensName = await ensNameOrAddress(this.addressValue);
-    this.element.innerText = ensName.replace(/^0x(.{5}).+(.{4})$/, '$1…$2');
+    this.element.innerText = ensName.replace(/^0x(.{5}).+(.{4})$/, '0x$1…$2');
   }
 }
